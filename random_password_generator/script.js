9f192a1415adddd52219a368c0c4e4d69072897a
Khaing Khant Htun <khaingkhanthtun@gmail.com>
2020-12-14 19:21:33 +0630

    random-password-generator-2

--- random_password_generator/script.js
+++ random_password_generator/script.js
@@ -1,3 +1,6 @@
+var alertBox = document.querySelector('.alertBox');
+alertBox.innerHTML = "Please Click 'Generate Password' button first";
+
 function getPassword() {
 	var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!A#$%^&*()_+?><:{}[]";
 	var passwordLength = 16;
@@ -8,4 +11,17 @@ function getPassword() {
 		password += chars.substring(randomNumber,randomNumber+1);
 	}
 	document.getElementById("password").value = password;
+	alertBox.innerHTML = "New Password Copied to clipboard! <br>"+ password;
+}
+
+
+function copyPassword() {
+	var copyPassText = document.getElementById('password');
+	copyPassText.select();
+	copyPassText.setSelectionRange(0,9999);
+	document.execCommand('copy');
+	alertBox.classList.toggle('active');
+	setTimeout(function() {
+		alertBox.classList.toggle('active');
+	}, 1800);
 }
